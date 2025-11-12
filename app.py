68e7701392a887f0c48ea1e625c9f891e3635762
JayPhate <jay.phate@gmail.com>
2025-11-12 17:09:10 +0000

    Restructred app file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from sym64!"
+    return "Hiya from sym64!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
